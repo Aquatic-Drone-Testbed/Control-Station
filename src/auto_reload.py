88993ae72443f162b7d3c93088a5b26340c26825
MarcuX009 <pk421933605@gmail.com>
2024-04-04 11:33:07 -0700

    using watchdog for hot reload

---
+++ src/auto_reload.py
@@ -0,0 +1,35 @@
+import sys
+import subprocess
+from watchdog.observers import Observer
+from watchdog.events import PatternMatchingEventHandler
+
+class ReloadEventHandler(PatternMatchingEventHandler):
+    def __init__(self, patterns, command):
+        super().__init__(patterns=patterns)
+        self.command = command
+        self.process = subprocess.Popen(self.command)
+
+    def on_modified(self, event):
+        self.process.kill()
+        self.process = subprocess.Popen(self.command)
+
+    def on_created(self, event):
+        self.process.kill()
+        self.process = subprocess.Popen(self.command)
+
+if __name__ == "__main__":
+    path = sys.argv[1]
+    command = sys.argv[2:]
+
+    event_handler = ReloadEventHandler(patterns=["*.py"], command=command)
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=True)
+    observer.start()
+
+    try:
+        observer.join()
+    except KeyboardInterrupt:
+        observer.stop()
+        event_handler.process.kill()
+
+    observer.join()
