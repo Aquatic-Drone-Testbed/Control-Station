515daf84c2970a4562c968f6e0f727600833d245
MarcuX009 <pk421933605@gmail.com>
2024-04-09 15:50:16 -0700

    able to rec gps data

---
+++ src/GPS_receiver.py
@@ -0,0 +1,21 @@
+# GPS-server.py
+import socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# get local machine name
+host = socket.gethostname()
+print(f"I am {host}")
+
+port = 9001 # GPS
+
+server_address = ('', port)
+sock.bind(server_address)
+
+print(f"Listening for messages from the boat-radio-2 on {port}...")
+
+while True:
+    data, address = sock.recvfrom(4096)
+
+    if data:
+        print(f"Received message: {data.decode()} from {address}")
